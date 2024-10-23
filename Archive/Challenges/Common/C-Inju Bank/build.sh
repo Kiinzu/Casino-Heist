fd216272a93a5d6248bc1d59b2c3882b06ff5f97
Richard Tan <richardmarchel86@gmail.com>
2024-10-23 21:44:56 +0700

    add Challenges

---
+++ Archive/Challenges/Common/C-Inju Bank/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Checking current directory
+output=$(pwd)
+if [[ "$output" ==  *challenge/images ]]; then
+    echo "Ready to Build!"
+else
+    echo "Moving to images"
+    cd images/
+fi
+
+# Building the docker for specific challenge
+./build.sh
+
+# Reseting the directory - challenge directory
+cd ../
+
+# Deploying the challenge instance
+docker-compose -p blockchain-inju-bank up  --build -d 
