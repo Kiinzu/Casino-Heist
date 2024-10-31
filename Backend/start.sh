ef795e3c35bb9d3c97edc132d7a31b5dc854d1a1
Richard Tan <richardmarchel86@gmail.com>
2024-10-31 07:19:04 +0700

    push new change for https deployment

--- Backend/start.sh
+++ Backend/start.sh
@@ -4,4 +4,3 @@ rm -rf ./instance/casino_heist.db
 pip3 install -r ./requirements.txt
 python3 database.py
 python3 insert_data.py
-python3 app.py
--- deploy_https.sh
+++ deploy_https.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+# initial
+echo "Generating 32-byte hex secret..."
+secret_key=$(openssl rand -hex 32)
+
+backend_env="./Backend/.env"
+echo "SECRET_KEY=$secret_key" > "$backend_env"
+
+echo "Secret key saved in $backend_env"
+
 # Step 1: Run docker-compose up in detached mode
 echo "Starting Docker containers..."
+docker-compose down
 docker-compose up -d
 
 # Step 2: Ask for domain name input
 read -p "Enter your domain name: " domain_name
 
-# Steo 3: Building the Frontend and moving it to the correct directory
+frontend_env="./Frontend/.env"
+echo "VITE_BACKEND_IP='https://$domain_name/api'" > "$frontend_env"
 cd ./Frontend
 ./install.sh
 cp -r ./dist/* /var/www/$domain_name/html
 cd ..
 
-# Step 4: Get the IP address of the 'react-casino-heist_web' container
+# Step 3: Get the IP address of the 'react-casino-heist_web' container
 container_id=$(docker ps --filter "ancestor=react-casino-heist_web" --format "{{.ID}}")
 
 if [ -z "$container_id" ]; then
@@ -23,8 +34,11 @@ fi
 
 echo "Container ID found: $container_id"
 
-# Step 5: Get the IP address of the selected container
+# Step 3: Get the IP address of the selected container
 container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$container_id")
+echo "$container_ip"
+echo "$conatiner_id"
+
 
 if [ -z "$container_ip" ]; then
     echo "Failed to get container IP. Make sure the container is running."
@@ -33,7 +47,7 @@ fi
 
 echo "Container IP for $container_name: $container_ip"
 
-# Step 6: Create the NGINX configuration file
+# Step 4: Create the NGINX configuration file
 nginx_config_path="/etc/nginx/sites-available/$domain_name"
 
 cat > "$nginx_config_path" <<EOL
@@ -78,18 +92,19 @@ server {
 }
 EOL
 
-# Step 7: Create symlink to enable the site
+# Step 5: Create symlink to enable the site
+rm -rf /etc/nginx/sites-enabled/$domain_name
 ln -s "$nginx_config_path" /etc/nginx/sites-enabled/
 
-# Step 8: Test NGINX configuration
+# Step 6: Test NGINX configuration
 nginx -t
 if [ $? -ne 0 ]; then
     echo "NGINX configuration test failed. Please check the config."
     exit 1
 fi
 
-# Step 9: Restart NGINX service
+# Step 7: Restart NGINX service
 echo "Restarting NGINX..."
-sudo service nginx restart #IF YOU ARE ON VPS PLEASE REMOVE THE SUDO, SOMETIME IT CAUSES ERROR, OR YOU CAN RESTART NGINX URSELF
+sudo service nginx restart
 
 echo "Deployment completed successfully!"
